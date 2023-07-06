af6c06ac10bfcac4263fc9f406ae607b693287b7
1nature <olanipekun1439@yahoo.com>
2023-07-06 09:51:31 +0100

    I removed var isGusessCorrect = randomWordToGuess.Contains(userInput). Instead I used only randomWordToGuess.Contains(userInput) in the condition

--- Another HangMan/Program.cs
+++ Another HangMan/Program.cs
@@ -6,83 +6,63 @@
         {
             Console.WriteLine("HangMan Project\n");
             const char PLACEHOLDER = '_';
-
-
-            List<string> wordsToGuess = new List<string>();
-
-            wordsToGuess.Add("supplier");
-            wordsToGuess.Add("employer");
-            wordsToGuess.Add("contractor");
-            wordsToGuess.Add("manager");
+            List<string> wordsToGuess = new List<string> { "employer", "supplier", "manager", "contractor" };
 
             Random random = new Random();
             int index = random.Next(wordsToGuess.Count);
             String randomWordToGuess = wordsToGuess[index];
             char[] guessedWord = new char[randomWordToGuess.Length];
-            
+
             int rightLetterCount = 0;
             int maxTriesLeft = randomWordToGuess.Length;
 
-
-            for (int y = 0; y < guessedWord.Length; y++)
+            for (int placeholderCount = 0; placeholderCount < guessedWord.Length; placeholderCount++)
             {
-                guessedWord[y] = PLACEHOLDER;
+                guessedWord[placeholderCount] = PLACEHOLDER;
             }
 
             Console.WriteLine(randomWordToGuess);
             Console.WriteLine(guessedWord);
             Console.WriteLine();
-            bool isGuessCorrect = false;
 
+            Console.WriteLine("Please enter a guess: \n");
             while (true)
             {
-                Console.WriteLine("Please enter a guess: \n");
                 char userInput = char.Parse(Console.ReadLine());
                 Console.WriteLine();
 
                 if (randomWordToGuess.Contains(userInput))
                 {
-                    isGuessCorrect = true;
-
-                }
-                else {isGuessCorrect = false;}
-
-                if (isGuessCorrect)
-                {
-                    for (int i = 0; i < randomWordToGuess.Length; i++)
+                    for (int rightGuessCount = 0; rightGuessCount < randomWordToGuess.Length; rightGuessCount++)
                     {
-                        if (userInput == randomWordToGuess[i])
+                        if (userInput == randomWordToGuess[rightGuessCount])
                         {
-                            
-                            guessedWord[i] = userInput;
-                            
-                            Console.WriteLine("Number of guesses made: {0}", rightLetterCount++);
-                            
+                            guessedWord[rightGuessCount] = userInput;
+           
+                            Console.WriteLine("Number of correctly guessed letters: {0}", ++rightLetterCount);
                         }
                     }
                     Console.WriteLine($"{userInput}: right guess");
                     Console.WriteLine(guessedWord);
                     Console.WriteLine("---------------------\n");
-
-
                     if (guessedWord.Length == rightLetterCount)
                     {
                         Console.WriteLine("You win");
+                        Console.WriteLine($"The hidden word is: {randomWordToGuess}");
                         break;
                     }
                 }
-                else if (!isGuessCorrect)
+                else if (!randomWordToGuess.Contains (userInput)) 
                 {
                     Console.WriteLine($"{userInput} is a wrong guess; try again");
-                    
-                    Console.WriteLine("Number of guesses left: {0}", maxTriesLeft--);
+
+                    Console.WriteLine("Number of guesses left: {0}", --maxTriesLeft);
 
                     if (maxTriesLeft == 0)
                     {
                         Console.WriteLine("You lose");
                         break;
                     }
-
                 }
             }
             Console.WriteLine("End of the game");
